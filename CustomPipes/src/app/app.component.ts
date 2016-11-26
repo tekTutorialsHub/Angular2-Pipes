83db8bdb0f0bd2067e454569b3bd55d0adf25c74
admin@tekTutorialsHub.com <admin@tekTutorialsHub.com>
2016-11-26 23:13:35 +0530

    First Commit

---
+++ CustomPipes/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent
+{
+
+    title: string = 'Angular 2 pipes Example' ;
+    toDate: Date = new Date();
+    msg: string= 'Welcome to Angular 2';
+    num: number= 9542.14554;
+    per: number= .7414;
+    cur: number= 175;
+}
+
+
+
